1cba81e7f4310cbce949a2c4c30a710ab6421d58
ivan-shork <494476749@qq.com>
2021-08-11 09:20:43 +0800

    初始化项目，安装依赖，创建android_app文件夹

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import TxCourse from './android_app/TxCourse.js';
+
+const App: () => Node = () => {
+
+  return (
+    <TxCourse />
+  );
+};
+
+
+export default App;
