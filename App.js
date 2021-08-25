a299e37633c1ee66e8d9854c3fce755016454de0
ivan <494476749@qq.com>
2021-08-25 11:00:44 +0800

    fix

--- App.js
+++ App.js
@@ -8,9 +8,6 @@
 
 import React from 'react';
 import './global';
-// import TxCourse from './android_app/TxCourse.js';
-import TodoList from './android_app/views/TodoList/index.js';
-import myTodoList from './android_app/config/myTodoList.js';
 import TxCourse from './android_app/navigation/txCourse';
 const App: () => Node = () => {
   return (
